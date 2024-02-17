6b6914331176ced3f68c048ab36f84bbdaab798b
Gautham Vanjre <vanjregautham@gmail.com>
2024-02-17 13:23:16 +0530

    feat: handled hydration error

--- app/components/HydrationDemo.tsx
+++ app/components/HydrationDemo.tsx
@@ -2,12 +2,14 @@
 import React, { useEffect, useState } from "react";
 
 const HydrationDemo = () => {
+  const [isClient, setIsClient] = useState<boolean>();
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const i = Math.random();
-  return (
-    <>
-      <div>The random value of i is {i}</div>
-    </>
-  );
+  return <>{isClient && <div>The random value of i is {i}</div>}</>;
 };
 
 export default HydrationDemo;
